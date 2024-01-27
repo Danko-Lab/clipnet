e239cbca089fcbba6e7307f42daaf35cba56ae09
Adam Youlin He <ayh8@cornell.edu>
2024-01-27 04:14:38 -0500

    Update calculate_deepshap.py

--- calculate_deepshap.py
+++ calculate_deepshap.py
@@ -137,7 +137,6 @@ def main():
         for j in range(0, len(seqs_to_explain), batch_size):
             shap_values = explainers[i].shap_values(seqs_to_explain[j : j + batch_size])
             raw_explanations[i].append(shap_values)
-            print(len(raw_explanations[i]))
             gc.collect()
 
     concat_explanations = []
@@ -146,7 +145,6 @@ def main():
             np.concatenate([exp for exp in raw_explanations[k]], axis=1).sum(axis=0)
         )
 
-    print(np.array(concat_explanations).shape)
     mean_explanations = np.array(concat_explanations).mean(axis=0)
     scaled_explanations = mean_explanations * seqs_to_explain
 
