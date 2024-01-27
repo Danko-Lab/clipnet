9d959a3d207e3732e4fce40f896893d0da4a6802
Adam Youlin He <ayh8@cornell.edu>
2024-01-27 03:21:41 -0500

    Update calculate_deepshap.py

--- calculate_deepshap.py
+++ calculate_deepshap.py
@@ -138,7 +138,7 @@ def main():
                 f"Calculating for sequences {j} to {min(j+batch_size, len(seqs_to_explain))}"
             )
             shap_values = explainer.shap_values(seqs_to_explain[j : j + batch_size])
-            print(shap_values.shape)
+            print(len(shap_values))
             raw_explanations[i].append(shap_values)
             gc.collect()
 
