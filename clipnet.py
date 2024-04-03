c7d32fc52e76123c40987ee792e99fa673bfa6c2
Adam Youlin He <ayh8@cornell.edu>
2024-04-03 03:46:00 -0400

    Update clipnet.py

--- clipnet.py
+++ clipnet.py
@@ -304,7 +304,7 @@ class CLIPNET:
                 np.concatenate([chunk[1] for chunk in y_predict_handle], axis=0),
             ]
         else:
-            y_predict = model.predict(X, batch_size=256, verbose=2)
+            y_predict = model.predict(X, batch_size=self.nn.batch_size, verbose=2)
         return y_predict
 
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
