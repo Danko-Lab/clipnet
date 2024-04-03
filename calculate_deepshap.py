9f64ca5dc067fc14b0027202fbbc4aed37ee14bf
Adam Youlin He <ayh8@cornell.edu>
2024-04-03 03:25:23 -0400

    shut up tensorflow

--- calculate_deepshap.py
+++ calculate_deepshap.py
@@ -4,6 +4,9 @@ Calculate contribution scores using shap.DeepExplainer.
 
 import argparse
 import gc
+import os
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"  # or any {'0', '1', '2'}
 
 import numpy as np
 import pyfastx
