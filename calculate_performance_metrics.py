1db965a2d9c5367eae7308e25f13083ad0a38fc3
Adam Youlin He <ayh8@cornell.edu>
2024-04-05 07:26:12 -0400

    Update calculate_performance_metrics.py

--- calculate_performance_metrics.py
+++ calculate_performance_metrics.py
@@ -9,7 +9,7 @@ import argparse
 import h5py
 import numpy as np
 import pandas as pd
-from scipy.spatial import jensenshannon
+from scipy.spatial.distance import jensenshannon
 from scipy.stats import pearsonr, spearmanr
 
 
