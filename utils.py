6fc100c7bfc16dd9ca162889b09fdd3ce166c472
Adam Youlin He <ayh8@cornell.edu>
2024-06-05 20:33:18 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -398,7 +398,7 @@ def l2_score(x, y):
     return np.sqrt(np.sum(np.square(x - y), axis=1))
 
 
-def plot_side(arr, ylim=[-2, 2.5], yticks=[0, 2], pic_name=None):
+def plot_side(arr, ylim=[-2, 2.5], yticks=[0, 2], xticks=[], pic_name=None):
     """
     Adapted from APARENT code (Bogard et al. 2019)
     """
@@ -416,7 +416,7 @@ def plot_side(arr, ylim=[-2, 2.5], yticks=[0, 2], pic_name=None):
     axes = plt.gca()
     axes.set_ylim(ylim)
     axes.set_yticks(yticks)
-    axes.set_xticks([])
+    axes.set_xticks(xticks)
     axes.spines[["right", "top", "bottom"]].set_visible(False)
     plt.xlim(-0.5, pl.shape[0] - 0.5)
     axes.tick_params(labelleft=False)
