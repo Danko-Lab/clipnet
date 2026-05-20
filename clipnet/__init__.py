87cb96e032c11884374ad4236d0ce19e0cc03dbd
Adam Youlin He <adamyhe@gmail.com>
2026-05-20 13:40:19 -0700

    Added PyPI publishing

--- clipnet/__init__.py
+++ clipnet/__init__.py
@@ -1,4 +1,9 @@
 # __init__.py
 # Adam He <adamyhe@gmail.com>
 
-__version__ = "0.2.1"
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("clipnet")
+except PackageNotFoundError:
+    __version__ = "unknown"
